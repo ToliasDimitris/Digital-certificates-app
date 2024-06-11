3a257b2b55beb3365609fd3f023155d629ff62be
ToliasDimitris <45165974+ToliasDimitris@users.noreply.github.com>
2024-06-11 14:00:49 +0300

    Formatting

--- Db_model.sql
+++ Db_model.sql
@@ -1,54 +1,45 @@
-create table Users (
-	
+create table Users (	
 	publicKey 	varchar(100) primary key,			
 	nonce 		int,		
 	firstName	varchar(100),	
 	lastName 	varchar(100),	
 	email 		varchar(100) unique
-
 ); 
 
 create table Certificates (
-
-	id 					serial primary key,		
+	id 				serial primary key,		
 	issuedAt 			bigint unsigned,		
 	issuedBy 			varchar(100),		
 	issuedTo 			varchar(100),		
 	imgDataUrl	  		mediumtext ,	
 	hash	   			char(64) unique,	/* bytes of max length 64 */
 	signature			varchar(132) unique,	
-	contractAddress		varchar(100)		
-
+	contractAddress			varchar(100)
 );
 
 create table Contracts (
-
     contractAddress   varchar(100) primary key,
     ownerAddress      varchar(100),
     foreign key (ownerAddress) references Users(publicKey)
 );
 
 create table Requests (
-
-	id 				serial primary key,				
+	id 			serial primary key,				
 	certId	 		bigint unsigned not null,  
-    type            varchar(100), 			
+    	type            	varchar(100), 			
 	viewed			bit(1),				/*(0 : false , 1: true  )*/
-	reqTo	 		varchar(100), 				/*Foreign Key, redundant info to save from unecessary joins if there are no requests for user*/
+	reqTo	 		varchar(100), 			/*Foreign Key, redundant info to save from unecessary joins if there are no requests for user*/
 	reqFrom			varchar(100),
 	foreign key (certId) references Certificates(id)
-
 );
 
 create table Responses (
-
     id              bigint unsigned primary key,
     certId          bigint unsigned not null,  
     type            varchar(100),           
-    viewed          bit(1),             /*(0 : false , 1: true  )*/
-    reqTo           varchar(100),               /*Foreign Key, redundant info to save from unecessary joins if there are no requests for user*/
+    viewed          bit(1),             			/*(0 : false , 1: true  )*/
+    reqTo           varchar(100),               		/*Foreign Key, redundant info to save from unecessary joins if there are no requests for user*/
     reqFrom         varchar(100),
     foreign key (certId) references Certificates(id)
-
 );
 
